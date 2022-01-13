1567d25707efb2e3cc4bad252049762a54a81156
Mikhai Wilson <iam@mikhaiwilson.com>
2022-01-12 22:58:22 -0500

    Added programs 1-3 and main method

---
+++ Sorting Algorithms/ProgramClass.java
@@ -0,0 +1,17 @@
+public class ProgramClass {
+    public void program1(int[] inputArray) {
+
+    }
+
+    public void program2(int[] inputArray) {
+
+    }
+
+    public void program3(int[] inputArray) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
