610bd07394eac795a3626757f42ad4d293acc44e
Mikhai Wilson <iam@mikhaiwilson.com>
2022-01-17 20:43:19 -0500

    finished program1 without debug

--- Sorting Algorithms/ProgramClass.java
+++ Sorting Algorithms/ProgramClass.java
@@ -1,17 +1,33 @@
+import java.util.Arrays;
+
 public class ProgramClass {
-    public void program1(int[] inputArray) {
+    public static void program1(int[] inputArray) {
+        // bubble sort
 
+        for (int i = 0; i < inputArray.length - 1; i++) {
+            if (inputArray[i] > inputArray[i + 1]) {
+                int valueCache = inputArray[i + 1];
+                inputArray[i + 1] = inputArray[i];
+                inputArray[i] = valueCache;
+            }
+        }
+
+        printArray(inputArray);
     }
 
-    public void program2(int[] inputArray) {
-
+    public static void program2(int[] inputArray) {
+        // selection sort
     }
 
-    public void program3(int[] inputArray) {
-
+    public static void program3(int[] inputArray) {
+        // insertion sort
     }
+
+    public static void printArray(int[] inputArray) {
+		System.out.println(Arrays.toString(inputArray));
+	}
 
     public static void main(String[] args) {
-        
+        program1()
     }
 }
