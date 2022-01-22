b106f0376a516984202da5242019e59d450063b5
mikhaiwilson <iam@mikhaiwilson.com>
2022-01-22 10:44:18 -0500

    complete program 1 draft

--- Sorting Strings/ProgramClass.java
+++ Sorting Strings/ProgramClass.java
@@ -1,3 +1,30 @@
+import java.util.Arrays;
+
 public class ProgramClass {
-    
+    public static void program1(String[] inputArray) {
+        for (int a = 0; a < inputArray.length; a++) {
+            for (int b = 0; b < inputArray.length - 1; b++) {
+                if (inputArray[b].compareTo(inputArray[b + 1]) > 0) {
+                    String valueCache = inputArray[b + 1];
+                    inputArray[b + 1] = inputArray[b];
+                    inputArray[b] = valueCache;
+                }
+            }
+        }
+
+        printArray(inputArray);
+    }
+
+    public static void program2(String[] inputArray) {
+
+    }
+
+    public static void printArray(String[] inputArray) {
+		System.out.println(Arrays.toString(inputArray));
+	}
+
+    public static void main(String[] args) {
+        String[] array1 = {"Zed", "Yield", "Chicklen", "Noob", "Xavier"}; 
+        program1(array1);
+    }
 }
