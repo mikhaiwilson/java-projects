2f91d1cfa18928538fdef3c46d76582a961480c2
mikhaiwilson <iam@mikhaiwilson.com>
2022-01-28 17:31:02 -0500

    fix indentations

--- If Statements/GradeConversions.java
+++ If Statements/GradeConversions.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
 public class GradeConversions {
-public static void main(String[] args) {
-    Scanner userInput = new Scanner(System.in);
-    int userInputInt = userInput.nextInt();
-    if (userInputInt >= 90){
-        System.out.print("Letter grade: A");
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        int userInputInt = userInput.nextInt();
+        if (userInputInt >= 90){
+            System.out.print("Letter grade: A");
+        }
+        else if (userInputInt >= 80){
+            System.out.print("Letter grade: B");
+        }
+        else if (userInputInt >= 70){
+            System.out.print("Letter grade: C");
+        }
+        else if (userInputInt >= 60){
+            System.out.print("Letter grade: D");
+        }
+        else if (userInputInt >= 0){
+            System.out.print("Letter grade: F");
+        }
+        else {
+            System.out.print("Numerical grade cannot be less than 0");
+        }
+        userInput.close();
     }
-    else if (userInputInt >= 80){
-        System.out.print("Letter grade: B");
-    }
-    else if (userInputInt >= 70){
-        System.out.print("Letter grade: C");
-    }
-    else if (userInputInt >= 60){
-        System.out.print("Letter grade: D");
-    }
-    else if (userInputInt >= 0){
-        System.out.print("Letter grade: F");
-    }
-    else {
-        System.out.print("Numerical grade cannot be less than 0");
-    }
-    userInput.close();
-  }
 }
