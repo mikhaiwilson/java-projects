308fa6ad7cb858feb56b5e4ecc66e578972a1aa0
mikhaiwilson <iam@mikhaiwilson.com>
2022-02-10 19:33:57 -0500

    add program1

--- ArrayList Methods/ProgramClass.java
+++ ArrayList Methods/ProgramClass.java
@@ -2,5 +2,11 @@ public class ProgramClass {
     // make sure that main class is first
     // make sure that printing is done in the main method
 
-    
+    public static void main(String[] args) {
+
+    } 
+
+    public static void program1() {
+        
+    }
 }
